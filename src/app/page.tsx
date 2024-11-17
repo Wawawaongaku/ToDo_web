5372516bc142a850909926e520abf64dc22bcbd5
Wawawaongaku <wata7872ame@gmail.com>
2024-11-17 17:46:29 +0900

    feat: ktpc semina-

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <div className={styles.page}>
             <main className={styles.main}>
-                <h1 className={styles.title}>Welcome to ToDo List</h1>
+                <h1 className={styles.title}>KTPC PC Seminar</h1>
                 <p className={styles.description}>
                     A simple ToDo list app
                 </p>
